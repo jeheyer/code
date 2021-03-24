c065e711ee3d13a4b69883568673d605792c6fe0
J5 <john.heyer@hightail.com>
2021-03-24 08:53:45 -0700

    asd

--- python/makerest.py
+++ python/makerest.py
@@ -13,8 +13,8 @@ def main(request):
         if "geoip" in request['path']:
             from geoip import GeoIP
             ipv4_address = request['path'].split("/")[2]
-            #if not ipv4_address:
-            ipv4_address = request['client_ip']
+            if not ipv4_address:
+                ipv4_address = request['client_ip']
             return vars(GeoIP(ipv4_address))
 
         if "getdnsservers" in request['path']:
