71970989a0ca964dc35ddcea2edf2a779a9ad19e
J5 <john.heyer@hightail.com>
2021-05-02 10:44:51 -0700

    asd

--- python/cgi/graffiti_post.py
+++ python/cgi/graffiti_post.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 def main(db_name, wall, name, text):
+
+    import sys
+    sys.path.insert(1, '../lib/')
 
     # Get database configuration info
     from system_tools import GetConfig
