82846961a3ca78e67ba6eff73f38b01e8fab8966
J5 <john@j5.org>
2021-05-10 00:52:47 +0000

    test

--- python/main.py
+++ python/main.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from flask import Flask
+#import sys, os
+#sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'lib'))
 
-app = Flask(__name__)
+from lib.makejson import *
+from lib.http_utils import *
 
-@app.route('/')
+# WSGI entry point
+def application(environ, start_response):
 
-def index():
-    return 'Web App with Python Flask!'
+    import traceback, json
+
+    response_headers = [ ('Content-type', 'text/plain') ]
+
+    try:
+
+        http_request = vars(HTTPRequest(environ))
+        data = main(http_request)
+        output = json.dumps(data, indent=2, default=str)
+
+        response_headers = [
+            ('Access-Control-Allow-Origin', '*'),
+            ('Content-type', 'application/json'),
+            ('Content-Length', str(len(output))),
+            ('Cache-Control', 'no-cache, no-store'),
+            ('Pragma', 'no-cache')
+        ]
+        start_response('200 OK', response_headers)
+        return [ output.encode('utf-8') ]
+
+    except:
+
+        start_response('500 Internal Server Error', response_headers)
+        return [ str(traceback.format_exc()).encode('utf-8') ]
