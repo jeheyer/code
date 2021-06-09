77527ee9dac2596a47dfca7f8fbc12fbc0b40517
J5 <nobody@j5.org>
2021-06-09 15:30:40 -0700

    updates

--- python/handle_post.py
+++ python/handle_post.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def application(environ, start_response):
+from flask import Flask, request, redirect
+from lib.web_apps import *
+
+# Flask entry point
+app = Flask(__name__)
+
+@app.route("/", defaults = {'path': ""}, methods=['POST'])
+@app.route("/<string:path>", methods=['POST'])
+@app.route("/<path:path>", methods=['POST'])
+
+def root(path):
 
     import traceback
 
     try:
 
-        x = "https://www.bastardboat.com/pollresults.html"
-        start_response(301, 'Location: ' + x)
-        return [ ]
+        inputs = {}
+        if "poll_vote" in path:
+            for _ in ['poll_db','poll_name','poll_desc','poll_url','choice_id']:
+                inputs[_] = request.form.get(_)
+            PollVote(inputs['poll_db'], inputs['poll_name'], inputs['choice_id'])
+            redirect_url = f"{inputs['poll_url']}?poll_name={inputs['poll_name']}&poll_desc={inputs['poll_desc']}"
+
+        if "graffiti_post" in path:
+            for _ in ['db_name','wall','name','text','graffiti_url']:
+                inputs[_] = request.form.get(_)
+            GraffitiPost(inputs['db_name'], inputs['wall'], inputs['name'], inputs['text'])
+            redirect_url = f"{inputs['graffiti_url']}?wall={inputs['wall']}"
+
+        return redirect(redirect_url)
 
     except:
+        return format(traceback.format_exc()), 500,  {'Content-Type': "text/plain"}
 
-        start_response('500 Internal Server Error', response_headers)
-        return [ str(traceback.format_exc()).encode('utf-8') ]
-
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ python/lib/web_apps.py
@@ -0,0 +1,42 @@
+def GraffitiPost(db_name: str, wall: str, name: str = "Anonymous Coward", text: str = "I have nothing to say", client_ip = None) -> bool:
+
+    # Get database configuration info
+    from lib.system_tools import GetConfig
+    db_info = GetConfig('mysql', db_name)
+    db_info['database'] = db_name
+
+    # Do database call
+    from lib.database import MySQLDatabase
+    mysql_database = MySQLDatabase(db_info)
+    mysql_database.OpenConnection()
+    table_name = "graffiti"
+    if client_ip:
+        sql_insert = f"INSERT INTO {table_name} (`wall`,`name`,`text`,`client_ip`) VALUES ('{wall}','{name}','{text}','{client_ip}';"
+    else:
+        sql_insert = f"INSERT INTO {table_name} (`wall`,`name`,`text`) VALUES ('{wall}','{name}','{text}');"
+    mysql_database.SQLQuery(sql_insert)
+    mysql_database.CloseConnection()
+
+def PollVote(db_name: str, poll_name: str, choice_id: int = 0) -> bool:
+
+    # Retrieve database configuration
+    from lib.system_tools import GetConfig
+    db_info = GetConfig('mysql', db_name)
+    db_info['database'] = db_name
+
+    # Connect to Database
+    from lib.database import MySQLDatabase
+    mysql_database = MySQLDatabase(db_info)
+    mysql_database.OpenConnection()
+    table_name = "polls"
+
+    sql_query = f"SELECT num_votes FROM {table_name} WHERE poll_name = '{poll_name}' AND choice_id = {choice_id}"
+    result = mysql_database.SQLQuery(sql_query)
+    if result:
+        num_votes = result[0]['num_votes'] + 1
+        sql_update = f"UPDATE {table_name} SET poll_name = '{poll_name}', choice_id={choice_id}, num_votes={num_votes} WHERE poll_name='{poll_name}' AND choice_id={choice_id}"
+        mysql_database.SQLQuery(sql_update)
+    else:
+        sql_insert = f"INSERT INTO {table_name} VALUES ('{poll_name}', {choice_id}, 1)"
+        mysql_database.SQLQuery(sql_insert)
+    mysql_database.CloseConnection()
