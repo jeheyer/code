defea36b960a7adba4bee06121b41d0ec06ccad3
J5 <nobody@j5.org>
2021-06-09 10:54:05 -0700

    asd

---
+++ python/handle_post.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def application(environ, start_response):
+
+    import traceback
+
+    try:
+
+        x = "https://www.bastardboat.com/pollresults.html"
+        start_response('301', 'Location: ' + x)
+        return [ ]
+
+    except:
+
+        start_response('500 Internal Server Error', response_headers)
+        return [ str(traceback.format_exc()).encode('utf-8') ]
+
