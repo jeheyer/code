b3aa063d32cf83a1cfb7e67fc41f501a9916f624
J5 <nobody@j5.org>
2021-06-11 13:55:23 -0700

    test

--- python/fastapi_driver.py
+++ python/fastapi_driver.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, Form
+from fastapi.responses import RedirectResponse
+from typing import Optional
 from lib.http_utils import *
 from lib.makejson import *
+from lib.web_apps import *
+import traceback
 
 app = FastAPI()
 
 # Route all possible paths here
-@app.get("/")
 @app.get("/{path:path}")
-def root(path, req: Request):
+def get_handler(path, req: Request):
 
-    from fastapi.responses import Response, JSONResponse
+    from fastapi.responses import JSONResponse
     from fastapi.encoders import jsonable_encoder
-    import traceback
 
     http_request = HTTPRequest()
     http_request.host = req.headers['host'].split(':')[0]
@@ -31,11 +33,53 @@ def root(path, req: Request):
             headers = {'Cache-Control': "no-cache, no-store", 'Pragma': "no-cache"},
             content =  jsonable_encoder(data)
         )
-        #return Response(
-        #    media_type = "application/json",
-        #    headers = {'Cache-Control': "no-cache, no-store", 'Pragma': "no-cache"},
-        #    content =  json.dumps(data, default=str)
-        #)
+
+    except:
+        return Response(status_code = 500, content = traceback.format_exc())
+
+@app.post("/graffiti_post")
+async def graffiti_post(
+    db_name: str = Form(...),
+    wall: str = Form(...),
+    graffiti_url: str = Form(...),
+    name: Optional[str] = Form("Anonymous Coward"),
+    text: Optional[str] = Form("I have nothing to say"),
+):
+
+    try:
+        GraffitiPost(db_name, wall, name, text)
+        redirect_url = f"{graffiti_url}?wall={wall}"
+        return RedirectResponse(url = redirect_url, status_code = 302)
+
+    except:
+        return Response(status_code = 500, content = traceback.format_exc())
+
+@app.get("/poll_vote")
+async def poll_vote(
+    poll_name: str = Form(...),
+    poll_url: str = Form(...),
+    poll_description: Optional[str] = Form("")
+):
+
+    try:
+        redirect_url = f"{poll_url}?poll_name={poll_name}&poll_desc={poll_desc}"
+        return RedirectResponse(url = redirect_url, status_code = 302)
+
+    except:
+        return Response(status_code = 500, content = traceback.format_exc())
+
+@app.post("/poll_vote")
+async def poll_vote(
+    poll_db: str = Form(...),
+    poll_name: str = Form(...),
+    poll_url: str = Form(...),
+    poll_description: Optional[str] = Form("")
+):
+
+    try:
+        PollVote(poll_db, poll_name, choice_id)
+        redirect_url = f"{poll_url}?poll_name={poll_name}&poll_desc={poll_desc}"
+        return RedirectResponse(url = redirect_url, status_code = 302)
 
     except:
         return Response(status_code = 500, content = traceback.format_exc())
