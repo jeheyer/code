6553eb41bbba2a3ab9cb8206c3a4086e0c53cd69
J5 <nobody@j5.org>
2021-06-11 14:10:59 -0700

    test

--- python/handle_post.py
+++ python/handle_post.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from paste import request
 from flask import Flask, request, redirect
 from lib.web_apps import *
 
@@ -13,14 +14,17 @@ def application(environ, start_response):
         path = environ.get('REQUEST_URI', '/').split('?')[0]
         inputs = {}
         if "poll_vote" in path:
+            fields = request.parse_formvars(environ)
             for _ in ['poll_db','poll_name','poll_desc','poll_url','choice_id']:
-                inputs[_] = request.form.get(_)
-            PollVote(inputs['poll_db'], inputs['poll_name'], inputs['choice_id'])
+                inputs[_] = fields.get(_, None)
+            if inputs['choice_id']:
+                PollVote(inputs['poll_db'], inputs['poll_name'], inputs['choice_id'])
             redirect_url = f"{inputs['poll_url']}?poll_name={inputs['poll_name']}&poll_desc={inputs['poll_desc']}"
 
         if "graffiti_post" in path:
+            fields = request.parse_formvars(environ)
             for _ in ['db_name','wall','name','text','graffiti_url']:
-                inputs[_] = request.form.get(_)
+                inputs[_] = fields.get(_, None)
             GraffitiPost(inputs['db_name'], inputs['wall'], inputs['name'], inputs['text'])
             redirect_url = f"{inputs['graffiti_url']}?wall={inputs['wall']}"
 
